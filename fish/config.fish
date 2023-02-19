e0a680e057d22f42591fea91b783c78b43b89f42
ivansaldarriaga <ivansaldarriaga@gmail.com>
2023-02-19 08:38:12 -0500

    make la to list with details

--- fish/config.fish
+++ fish/config.fish
@@ -98,7 +98,7 @@ end
 ## Useful aliases
 # Replace ls with exa
 alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias la='exa -al --color=always --group-directories-first --icons'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first --icons'  # long format
 alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
 alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
