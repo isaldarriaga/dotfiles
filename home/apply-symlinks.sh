6c7dff94999b1e431fdb8bd9d4957b17b1aed0e3
Ivan Saldarriaga <ivansaldarriaga@gmail.com>
2023-03-01 09:58:08 -0500

    latest changes

--- home/apply-symlinks.sh
+++
@@ -1,9 +0,0 @@
-PWD=$(pwd) && \
-
-symlink_folder() {
- echo && \
-SYM=~/.config/alacritty/alacritty && \
-SRC=~/repos/.dotfiles/home/.config/alacritty && \
-MSG="Symlinking $SYM ..." && echo && echo "$MSG" && \
-rm $SYM && mkdir --parents  
-}
