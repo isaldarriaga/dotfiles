934fd68e9413e71a7d1e85261c8317b593013437
ivan <ivansaldarriaga@gmail.com>
2024-02-07 19:57:28 -0500

    add permanent mason config

--- shared/home/.config/nvim/init.lua
+++ shared/home/.config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.keymaps")
+require("config.mason")
 -- TODO: position file explorer to right + autoclose
 -- TODO: position buffer explorer to left + autoclose
