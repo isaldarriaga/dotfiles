2190599842ba7415b62cc428c13105dff7ff4577
ivan <ivansaldarriaga@gmail.com>
2024-02-07 19:40:09 -0500

    fix lux config

--- shared/home/.config/nvim/init.lua
+++ shared/home/.config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.keymaps")
+-- TODO: position file explorer to right + autoclose
+-- TODO: position buffer explorer to left + autoclose
