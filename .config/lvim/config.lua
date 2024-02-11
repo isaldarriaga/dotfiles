aabb896df1c1ba60dae27210b13942ac17df6d7d
ivan <ivansaldarriaga@gmail.com>
2024-02-11 00:09:56 -0500

    control tab buffer navigation

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,3 +3,50 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- options
+-- vim.opt.relativenumber = true
+
+-- keymaps
+
+-- buffer navigation
+lvim.keys.normal_mode["<C-Tab>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.insert_mode["<C-Tab>"] = "<Esc>:BufferLineCycleNext<CR>"
+
+lvim.keys.normal_mode["<C-S-Tab>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.insert_mode["<C-S-Tab>"] = "<Esc>:BufferLineCyclePrev<CR>"
+-- close: <C-F4>
+
+
+-- close all: <C-S-F4>
+
+-- window
+
+-- navigation
+-- up: <C-Up>
+-- down: <C-Down>
+-- left: <C-Left>
+-- right: <C-Right>
+-- terminal: <C-S-t>
+
+-- resizing
+-- bigger: <C-S-Up>
+-- smaller: <C-S-Down>
+
+-- LSP
+-- go to definition: <F12>
+-- go to references: <S-F12>
+-- rename: F2
+
+-- INSERT MODE
+-- toggle case: <C-u>
+-- delete line: <C-S-l>
+-- save: <C-s>
+--
+-- MULTILINE
+-- add selection: <A-j>
+-- remove selectedL <A-S-j>
+-- select all: <C-S-A-j>
+
+-- NEOTREE
+-- rename: F2
