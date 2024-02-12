8e3ec991cb7402d6163bf8c6d8c331a67e3ef36c
ivan <ivansaldarriaga@gmail.com>
2024-02-11 20:42:58 -0500

    Close buffer: Ctrl + F4

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -9,40 +9,39 @@
 
 -- keymaps
 
--- buffer navigation
+-- Navigation
+
+-- next buffer: Ctrl + Tab
 lvim.keys.normal_mode["<C-Tab>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.insert_mode["<C-Tab>"] = "<Esc>:BufferLineCycleNext<CR>"
 
+-- prev buffer: Ctrl + Shift + Tab
 lvim.keys.normal_mode["<C-S-Tab>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.insert_mode["<C-S-Tab>"] = "<Esc>:BufferLineCyclePrev<CR>"
 
--- close: <C-F4>
--- lvim.keys.normal_mode["<C-F4>"] = ":BufferKill<CR>"
--- lvim.keys.insert_mode["<C-Tab>"] = "<Esc>:BufferKill<CR>"
+-- close buffer: Ctrl + F4
+lvim.keys.normal_mode["<F28>"] = ":BufferKill<CR>"
+lvim.keys.insert_mode["<F28>"] = "<Esc>:BufferKill<CR>"
 
--- close all: <C-S-F4>
-
--- window navigation
-
--- jump window up
+-- jump window up: Ctrl + Up
 lvim.keys.normal_mode["<C-Up>"] = "<C-w>k"
 lvim.keys.insert_mode["<C-Up>"] = "<Esc><C-w>k<CR>"
 
--- jump window down
+-- jump window down: Ctrl + Down
 lvim.keys.normal_mode["<C-Down>"] = "<C-w>j"
 lvim.keys.insert_mode["<C-Down>"] = "<Esc><C-w>j<CR>"
 
--- jump window left
+-- jump window left: Ctrl + Left
 lvim.keys.normal_mode["<C-Left>"] = "<C-w>h"
 lvim.keys.insert_mode["<C-Left>"] = "<Esc><C-w>h<CR>"
 
--- jump window right
+-- jump window right: Ctrl + Right
 lvim.keys.normal_mode["<C-Right>"] = "<C-w>l"
 lvim.keys.insert_mode["<C-Right>"] = "<Esc><C-w>l<CR>"
 
 -- terminal: <C-S-t>
 
--- resizing
+-- resize window: Ctrl + Shift + <direction>
 lvim.keys.normal_mode["<C-S-Up>"] = ":resize -2<CR>"
 lvim.keys.normal_mode["<C-S-Down>"] = ":resize +2<CR>"
 lvim.keys.normal_mode["<C-S-Left>"] = ":vertical resize -2<CR>"
@@ -58,15 +57,16 @@ lvim.keys.normal_mode["<C-S-Right>"] = ":vertical resize +2<CR>"
 -- toggle case: <C-u>
 -- delete line: <C-S-l>
 -- save: <C-s>
--- indenting <tab>
+
+-- indenting while selecting: Tab / Shift + Tab
 lvim.keys.visual_mode["<Tab>"] = ">gv"
 lvim.keys.visual_mode["<S-Tab>"] = "<gv"
 
--- move line up: <A-Up>
+-- move line up: Alt + Up
 lvim.keys.normal_mode["<A-Down>"] = ":m .+1<CR>=="
 lvim.keys.insert_mode["<A-Down>"] = "<Esc>:m .+1<CR>=="
 
--- move line down: <A-Down>
+-- move line down: Alt + Down
 lvim.keys.normal_mode["<A-Up>"] = ":m .-2<CR>=="
 lvim.keys.insert_mode["<A-Up>"] = "<Esc>:m .-2<CR>=="
 
@@ -112,7 +112,7 @@ lvim.plugins = {
       require("nvim-ts-autotag").setup()
     end,
   },
-  -- jump by searching with 2 chars
+  -- search by typing 2-3 chars of word
   {
     "ggandor/leap.nvim",
     name = "leap",
@@ -135,7 +135,7 @@ lvim.plugins = {
       })
     end,
   },
-  -- preview LSP definition, references, impl in floating window
+  -- preview definition, references, implementation with a floating window via LSP
   {
     "rmagatti/goto-preview",
     config = function()
@@ -154,15 +154,16 @@ lvim.plugins = {
       }
     end
   },
-  -- preview LSP signatures
+  -- preview function signature via LSP
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require "lsp_signature".on_attach() end,
   },
-  -- diagnostics
+  -- diagnostics window
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
+  -- 
 }
