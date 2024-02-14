d713cd0c093d8e46f4a6f8d3f17c75fb4e66e70e
ivan <ivansaldarriaga@gmail.com>
2024-02-13 23:32:28 -0500

    use lunar

--- .config/lunarvim/config.lua
+++ .config/lunarvim/config.lua
@@ -3,6 +3,8 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+--
+-- lvim.colorscheme = "tokionight"
 
 -- options
 -- vim.opt.relativenumber = true
@@ -57,6 +59,7 @@ lvim.keys.normal_mode["<C-S-Right>"] = ":vertical resize +2<CR>"
 -- toggle case: <C-u>
 -- delete line: <C-S-l>
 -- save: <C-s>
+-- move to next line after comment
 
 -- indenting while selecting: Tab / Shift + Tab
 lvim.keys.visual_mode["<Tab>"] = ">gv"
@@ -165,5 +168,13 @@ lvim.plugins = {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
-  -- 
+  -- tokionight
+  -- {
+    -- "folke/tokionight.vim"
+  -- },
 }
+
+-- vim.g.tokyonight_style = "storm" -- can be storm, night, or day
+-- lvim.colorscheme = 'tokyonight'
+--
+lvim.colorscheme = "lunar"
